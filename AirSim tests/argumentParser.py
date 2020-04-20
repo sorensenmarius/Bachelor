1c5dae6948365cc77cd0cff805625a0cd7a5cc55
Odin <odinbjoernebo@gmail.com>
2020-04-20 14:57:54 +0200

    added help string that will be displayed on all arguments when argumentparser.py with argument -h or --help is run

--- AirSim tests/argumentParser.py
+++ AirSim tests/argumentParser.py
@@ -9,20 +9,26 @@ pathDefFolder = "defFolder"
 def parseCLIargs():
     parser = argparse.ArgumentParser("Parses arguments from cli")
 
-    parser.add_argument("-sl","-save_lidar",default=False ,action="store_true")
+    parser.add_argument("-sl","-save_lidar",default=False ,action="store_true", help="stores all \
+    data when running the script: positional, lidar and photos")
     
-    parser.add_argument("-saveImage",default=False ,action="store_true")
-    parser.add_argument("-saveImageFolder",nargs=1) #With foldername location
-    parser.add_argument("-savePos", default=False, action="store_true")
-    parser.add_argument("-savePosFile", nargs=1) #With filename location
+    parser.add_argument("-saveImage",default=False ,action="store_true", help="Makes images taken \
+    during the flight be saved in a folder")
+    parser.add_argument("-saveImageFolder",nargs=1, help= "Makes images taken during flight be \
+    saved to a specific folder") 
+    parser.add_argument("-savePos", default=False, action="store_true", help="Makes positions \
+    captured during flight be saved to a folder")
+    parser.add_argument("-savePosFile", nargs=1, help= "Makes positions captured \
+    during flight be saved to a specific folder")
 
     parser.add_argument("-points", nargs=3,type= int,help="Specifies a point the \
-    drone will try to reach. Format: -points x y z")
-    parser.add_argument("-path",nargs=1,help="Specifies a path the \
-    drone will try to reach from a given file. Format: -path filename")
+    drone will try to reach during the simulation")
+    parser.add_argument("-path",nargs=1,help="Specifies a path of goals the \
+    drone will try to reach from a given file")
+
 
     args = parser.parse_args() #Makes the namespace dict that will be returned
-    
+
     if args.points == None and args.path == None:
         args.points = pointDef
         args.followPath = False
